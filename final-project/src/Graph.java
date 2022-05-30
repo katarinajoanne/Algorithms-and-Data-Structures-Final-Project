0a02c882434c4502e9e6e69029483b881d9fbb8f
vaniaclementine <vaniaclementinee@gmail.com>
2022-05-31 01:11:41 +0700

    fixed graph output

--- final-project/src/Graph.java
+++ final-project/src/Graph.java
@@ -15,7 +15,7 @@ public class Graph {
     public void displayGraph() {
         // To make graph more readable
         System.out.print(" ");
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < numVertex; i++) {
             System.out.printf("%5d", i);
         }
 
